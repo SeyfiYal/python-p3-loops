7cc52325bfefe3b5f6cbc10368ba08a44cb918d2
Seyfican Yalcindag <+>
2023-04-03 18:38:54 +0300

    loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
 
-def happy_new_year():
-    # code goes here!
-    pass
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+def happy_new_year():
+    count = 10
+    while count > 0:
+        print(count)
+        count -= 1
+    print("Happy New Year!")
+happy_new_year()
+
+
+def square_integers(lst):
+    result = []
+    for num in lst:
+        if isinstance(num, int):
+            result.append(num ** 2)
+    return result
+
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
